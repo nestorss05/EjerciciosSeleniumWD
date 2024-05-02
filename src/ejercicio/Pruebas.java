391ddefedce7a13fae3b58d1f602c6029be01413
nestorss05 <nestor.sanchez@iesnervion.es>
2024-05-02 17:26:36 +0200

    Ejercicio 3 FINAL

--- src/ejercicio/Pruebas.java
+++ src/ejercicio/Pruebas.java
@@ -27,54 +27,56 @@ class Pruebas {
 
 	@BeforeEach
 	void visitaPagina() {
-		driver1.get("https://elenarivero.github.io/Ejercicio2/index.html");        
+		driver1.get("https://elenarivero.github.io/ejercicio3/index.html");        
 	}
 	
 	private static Stream<Arguments> testOK() {
 		return Stream.of(
-				Arguments.of("333","+"), // Test 1
-				Arguments.of("001","+"), // Test 10
-				Arguments.of("002","+"), // Test 11
-				Arguments.of("998","+"), // Test 12
-				Arguments.of("999","+") // Test 13
+				Arguments.of("Felipe Filipino","16/04/2006"), // Test 1
+				Arguments.of("Felipe Filipino","01/03/2006"), // Test 12
+				Arguments.of("Felipe Filipino","02/03/2006"), // Test 13
+				Arguments.of("Felipe Filipino","30/03/2006"), // Test 14
+				Arguments.of("Felipe Filipino","31/03/2006"), // Test 15
+				Arguments.of("Felipe Filipino","16/01/2006"), // Test 20
+				Arguments.of("Felipe Filipino","16/02/2006"), // Test 21
+				Arguments.of("Felipe Filipino","16/11/2006"), // Test 22
+				Arguments.of("Felipe Filipino","16/12/2006") // Test 23
 			);
 	}
 	
 	private static Stream<Arguments> testFailA() {
 		return Stream.of(
-				Arguments.of("33A","+"), // Test 2
-				Arguments.of("33_","+"), // Test 3
-				Arguments.of("3","+"), // Test 4
-				Arguments.of("33333","+"), // Test 5
-				Arguments.of("44","+"), // Test 6
-				Arguments.of("4444","+"), // Test 7
-				Arguments.of("-222","+"), // Test 8
-				Arguments.of("000","+"), // Test 9: Falla: El programa solo detecta si hay 3 caracteres.
-				Arguments.of("1000","+"), // Test 14
-				Arguments.of("","+") // Test 15
+				Arguments.of("","16/04/2006"), // Test 2
+				Arguments.of("5elipe Filipino","16/04/2006"), // Test 3
+				Arguments.of("*elipe Filipino","16/04/2006") // Test 4
 			);
 	}
 	
 	private static Stream<Arguments> testFailB() {
 		return Stream.of(
-				Arguments.of("333",""), // Test 16
-				Arguments.of("333","+++"), // Test 17: Falla: El programa no detecta si hay 2 o mas simbolos en el campo.
-				Arguments.of("333","++"), // Test 18: Falla: El programa no detecta si hay 2 o mas simbolos en el campo.
-				Arguments.of("333","A"), // Test 19
-				Arguments.of("333","4"), // Test 20
-				Arguments.of("333", "_") // Test 21
+				Arguments.of("Felipe Filipino",""), // Test 5
+				Arguments.of("Felipe Filipino","16-04-2006"), // Test 6
+				Arguments.of("Felipe Filipino","16/04/2006.5"), // Test 7
+				Arguments.of("Felipe Filipino","-05/04/2006"), // Test 8
+				Arguments.of("Felipe Filipino","33/04/2006"), // Test 9 - Falla porque no se controlan si los dias son mayores que 31
+				Arguments.of("Felipe Filipino", "00/03/2006"), // Test 10
+				Arguments.of("Felipe Filipino", "32/03/2006"), // Test 16 - Falla porque no se controlan si los dias son mayores que 31
+				Arguments.of("Felipe Filipino", "16/-04/2006"), // Test 17
+				Arguments.of("Felipe Filipino", "16/33/2006"), // Test 18
+				Arguments.of("Felipe Filipino", "16/00/2006"), // Test 19
+				Arguments.of("Felipe Filipino", "16/13/2006") // Test 24
 			);
 	}
 	
 	@ParameterizedTest
 	@MethodSource("testOK")
-	void testOK(String numEmp, String directivo) {
+	void testOK(String nombreApellidos, String fechaNac) {
 		
-		WebElement inputEmpleado = driver1.findElement(By.id("numero"));
-		inputEmpleado.sendKeys(numEmp);
+		WebElement inputNombreApellidos = driver1.findElement(By.id("nomap"));
+		inputNombreApellidos.sendKeys(nombreApellidos);
 		
-		WebElement inputDirectivo = driver1.findElement(By.id("directivo"));
-		inputDirectivo.sendKeys(directivo);
+		WebElement inputFechaNac = driver1.findElement(By.id("fecha"));
+		inputFechaNac.sendKeys(fechaNac);
 		
 		WebElement submit = driver1.findElement(By.xpath("//input[@type='submit']"));
 		submit.click();
@@ -87,44 +89,44 @@ class Pruebas {
 	
 	@ParameterizedTest
 	@MethodSource("testFailA")
-	void testFailA(String numEmp, String directivo) {
+	void testFailA(String nombreApellidos, String fechaNac) {
 		
-		WebElement inputEmpleado = driver1.findElement(By.id("numero"));
-		inputEmpleado.sendKeys(numEmp);
+		WebElement inputNombreApellidos = driver1.findElement(By.id("nomap"));
+		inputNombreApellidos.sendKeys(nombreApellidos);
 		
-		WebElement inputDirectivo = driver1.findElement(By.id("directivo"));
-		inputDirectivo.sendKeys(directivo);
+		WebElement inputFechaNac = driver1.findElement(By.id("fecha"));
+		inputFechaNac.sendKeys(fechaNac);
 		
 		WebElement submit = driver1.findElement(By.xpath("//input[@type='submit']"));
 		submit.click();
 		
-		WebElement errorNumero = driver1.findElement(By.id("errorNumero"));
+		WebElement errorNumero = driver1.findElement(By.id("errorNomap"));
 		boolean errorN = errorNumero.isDisplayed();
 		assertTrue(errorN);
 		
-		WebElement errorDirectivo = driver1.findElement(By.id("errorDirectivo"));
+		WebElement errorDirectivo = driver1.findElement(By.id("errorFecha"));
 		boolean errorD = errorDirectivo.isDisplayed();
 		assertFalse(errorD);
 	}
 	
 	@ParameterizedTest
 	@MethodSource("testFailB")
-	void testFailB(String numEmp, String directivo) {
+	void testFailB(String nombreApellidos, String fechaNac) {
 		
-		WebElement inputEmpleado = driver1.findElement(By.id("numero"));
-		inputEmpleado.sendKeys(numEmp);
+		WebElement inputNombreApellidos = driver1.findElement(By.id("nomap"));
+		inputNombreApellidos.sendKeys(nombreApellidos);
 		
-		WebElement inputDirectivo = driver1.findElement(By.id("directivo"));
-		inputDirectivo.sendKeys(directivo);
+		WebElement inputFechaNac = driver1.findElement(By.id("fecha"));
+		inputFechaNac.sendKeys(fechaNac);
 		
 		WebElement submit = driver1.findElement(By.xpath("//input[@type='submit']"));
 		submit.click();
 		
-		WebElement errorNumero = driver1.findElement(By.id("errorNumero"));
+		WebElement errorNumero = driver1.findElement(By.id("errorNomap"));
 		boolean errorN = errorNumero.isDisplayed();
 		assertFalse(errorN);
 		
-		WebElement errorDirectivo = driver1.findElement(By.id("errorDirectivo"));
+		WebElement errorDirectivo = driver1.findElement(By.id("errorFecha"));
 		boolean errorD = errorDirectivo.isDisplayed();
 		assertTrue(errorD);
 	}
